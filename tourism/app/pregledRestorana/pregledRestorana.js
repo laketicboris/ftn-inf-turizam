e4128b1e5bf02590807311f3d560e70c4ccbf093
Josip Valtner <josipvaltner@gmail.com>
2025-05-05 18:33:19 +0200

    proba1

---
+++ tourism/app/pregledRestorana/pregledRestorana.js
@@ -0,0 +1,90 @@
+`use strict`
+class Jelo{
+    constructor(naziv, opis, tip){
+        this.naziv = naziv
+        this.opis = opis
+        this.tip = tip
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', initializeJela)
+
+function initializeJela() {
+    //JSON
+    let jela = []
+    let jelaString = localStorage.getItem("jela")
+    if(jelaString) {
+        jela = JSON.parse(jelaString)
+    }
+
+    createProductRows(jela)
+    loadTabeleStyles(jela)
+    saveLocalStorage(jela)
+  }
+
+function loadTabeleStyles(){
+    let tabela = document.querySelector("#jela") 
+    tabela.style.borderCollapse = "collapse"
+    tabela.style.border = "1px solid black"
+    tabela.style.textAlign = "center"
+    let ths = document.querySelectorAll("th")
+    for (let th of ths) {
+    th.style.border = "1px solid black"
+    }
+
+    let tds = document.querySelectorAll("td")
+    for (let td of tds) {
+        td.style.border = "1px solid black"
+        td.style.padding = "5px"
+    }
+}
+function createProductRows(jela){
+    let table = document.querySelector("#tbody-jela")
+    table.innerHTML = ''
+
+    for(let jelo of jela){
+        let tabela = document.querySelector("#tbody-jela") 
+        let tr = document.createElement("tr")
+    
+        
+        let naziv= document.createElement("td") 
+        let tip= document.createElement("td")
+        
+        naziv.textContent =jelo.naziv
+        tip.textContent = jelo.tip 
+        
+        tr.appendChild(naziv)
+        tr.appendChild(tip)
+        tr.addEventListener('click',
+            function (event) {
+                    displayProductDetails(jelo)
+                    event.stopPropagation()
+             })
+        tabela.appendChild(tr)
+    }
+}
+function displayProductDetails(jelo) {
+    
+        let tabela = document.querySelector("#prikaziDetalje") 
+        let tr = document.createElement("tr")
+    
+        
+        let naziv= document.createElement("td") 
+        let tip= document.createElement("td") 
+        let opis= document.createElement("td") 
+        
+        naziv.textContent = "Naziv: " + jelo.naziv
+        tip.textContent = "Tip: " + jelo.tip 
+        opis.textContent = "Opis: " + jelo.opis 
+        
+        tr.appendChild(naziv)
+        tr.appendChild(tip)
+        tr.appendChild(opis)
+        tabela.appendChild(tr)
+}
+
+function saveLocalStorage(jela){
+    let jelaJSON =JSON.stringify(jela)
+    localStorage.setItem("jela", jelaJSON)
+}
