82b93a650ba9479de905bd5b08798a48464a6312
Pera Peric <pera.peric@gmail.com>
2025-05-06 18:25:43 +0200

    Dodata funkcionalnost za dodavanje tura i implementiran rad sa lokalStorage-om

--- tourism/app/tura/tura.js
+++ tourism/app/tura/tura.js
@@ -37,12 +37,74 @@ function prikaziDetaljeTure(tura){
 
     detaljiDiv.appendChild(p)
 }
+function sacuvajTureULocalStorage() {
+    localStorage.setItem("ture", JSON.stringify(ture));
+  }
+
 function inicijalizujTure(){
-    ture = [
+    const sacuvaneTure= localStorage.getItem("ture")
+
+    if(sacuvaneTure){
+        const niz = JSON.parse(sacuvaneTure)
+        ture = niz.map(t => new Tura(t.naziv, t.duzina, t.opis, t.tagovi))
+    }
+    else
+    {
+        ture = [
         new Tura("Gradska šetnja", 5, "Vođena tura kroz centar grada.", ["gradska", "istorijska"]),
         new Tura("Planinska avantura", 12, "Pešačka tura sa prelepim pogledom.", ["priroda", "planinarenje"]),
         new Tura("Kulturna ekspedicija", 8, "Muzeji, galerije i spomenici kulture.", ["kultura", "umetnost"])
-     ]
-     prikazTure();
+        ]
+        sacuvajTureULocalStorage();
+    }
+    prikazTure();
 }
-document.addEventListener("DOMContentLoaded", inicijalizujTure)
+function obradiFormu(){
+    const forma = document.getElementById("forma")
+
+    forma.addEventListener("submit", function(event){
+        event.preventDefault()
+
+        const podaci = new FormData(forma)
+        const naziv = podaci.get("naziv")
+        const duzina = podaci.get("duzina")
+        const opis = podaci.get("opis")
+
+        const unosTaga = document.querySelectorAll('input[name="tagovi"]')
+        const tagovi = Array.from(unosTaga).map(input => input.value.trim()).filter(tag => tag !== "")
+
+        const novaTura = new Tura (naziv, duzina, opis, tagovi)
+        ture.push(novaTura)
+
+        sacuvajTureULocalStorage()
+        prikazTure()
+        forma.reset();
+
+        document.getElementById("tagContainer").innerHTML = `
+        <label>Tagovi (npr: gradska, istorijska):</label>
+        <div class="tag-unos">
+        <input type="text" name="tagovi" placeholder="Unesite tag">
+        <button type="button" onclick="addTagField()">+</button>
+        </div>`
+    })
+}
+function dodajTagPolje(){
+    const tagContainer = document.getElementById("tagContainer")
+
+    const novaGrupa = document.createElement("div")
+    novaGrupa.className = "tag-unos"
+
+    const noviUnos = document.createElement("input")
+    noviUnos.type = "text"
+    noviUnos.name = "tagovi"
+    noviUnos.placeholder = "Unesite tag"
+
+    novaGrupa.appendChild(noviUnos)
+    tagContainer.appendChild(novaGrupa)
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    inicijalizujTure()
+    obradiFormu()
+})
